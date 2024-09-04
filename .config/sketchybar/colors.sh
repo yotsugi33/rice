311dc19508c1f8735c00e8112b2df40ed444dbf9
ogordev <ogordev@proton.me>
2024-09-04 12:50:50 +0700

    updated code

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff000000
+export WHITE=0xffffffff
+export RED=0xfff44336
+export GREEN=0xffa6d189
+export BLUE=0xff99d1db
+export YELLOW=0xfffff9c4
+export ORANGE=0xffef9f76
+export MAGENTA=0xffe1bee7
+export GREY=0xffede7f6
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0x70d1c4e9
+export BAR_BORDER_COLOR=0xd9ca9ee6 #0xff80cbc4 #0xa024273a
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0xba7c7f93
+export BACKGROUND_2=0xfffff9c4
+
+export POPUP_BACKGROUND_COLOR=0xff1e1e2e
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
+
+
+# black 0xff000000
+#
